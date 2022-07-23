a455b86d67d77fef08851bc72a23adb226ca5136
MariannaVIN <semenova.m@gmail.com>
2022-07-23 19:47:39 +0300

    done for 1 entering

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать прогамму, которая из имеющегося массива строк 
+// формирует массив из строк, длина которых меньше либо равна 3 символам.
+
+Console.WriteLine("Введите символы в строку:");
+string Number = Console.ReadLine();
+
+void Quantity3 (string Number)
+{
+    int count = Number.Length;
+    if(Number.Length <=3)
+    {Console.WriteLine(Number);}
+    else 
+    {Console.WriteLine(" ");}
+}
+
+Quantity3 (Number);
